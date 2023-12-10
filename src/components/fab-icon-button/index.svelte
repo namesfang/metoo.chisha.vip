4bb78fc1db065dcfea12db0b06c6d1f1a75b5a62
namo <jixiang_f@163.com>
2023-12-10 21:43:03 +0800

    基础组件

---
+++ src/components/fab-icon-button/index.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import FabIcon from '$components/fab-icon/index.svelte';
+	import { createEventDispatcher } from 'svelte';
+
+  enum ButtonType {
+    button = 'button',
+    submit = 'submit',
+    reset = 'reset'
+  }
+
+  let className = '';
+  
+  const dispatch = createEventDispatcher();
+  const click = ()=> {
+    dispatch('click')
+  };
+
+  export let title = '';
+  export let type = ButtonType.button;
+  export let icon: string;
+  export { className as class }
+
+  $: className = `${className} fab-icon-button`
+</script>
+
+<button on:click={ click } {type} class={className} {title}>
+  <FabIcon type={icon}/>
+</button>
+
+<style lang="scss">
+  .fab-icon-button {
+    background-color: transparent;
+    border: 0;
+  }
+</style>
