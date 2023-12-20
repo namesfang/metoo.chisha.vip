258a2a73824599b82fdf14779968afc54e24e85a
Namo <namo.fang@ixiaoshuidi.com>
2023-12-20 13:37:34 +0800

    组件

--- src/routes/party/+page.svelte
+++ src/routes/party/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import FabIcon from '$components/fab-icon/index.svelte';
+  import FabButton from '$components/fab-button/index.svelte';
 	import { PUBLIC_STATIC_URL } from '$env/static/public';
 
   let parties = [
@@ -28,35 +29,42 @@
       <span>152****4999</span>
     </div>
     <div class="et">
-      <a href="/logout">
-        <FabIcon type="shield-check"/>
-      </a>
+      <FabButton icon="out" href="/logout"/>
     </div>
   </div>
-  <div class=""></div>
+  <div class="party-wrapper">
+    <div class="header">
+      <h2>列表</h2>
+      <nav>
+        <FabButton icon="plus" link href="/party/join">加入</FabButton>
+        <FabButton icon="plus" link href="/party/create">创建</FabButton>
+      </nav>
+    </div>
+    <ul>
+      {#each parties as t}
+        <li>
+          <a href="/metoo/party/{t.id}">{ t.name }</a>
+        </li>
+      {/each}
+    </ul>
+  </div>
 </div>
 
-<ul>
-  {#each parties as t}
-    <li>
-      <a href="/metoo/party/{t.id}">{ t.name }</a>
-    </li>
-  {/each}
-</ul>
-
-<a href="/metoo/party/join">加入派对</a>
-<a href="/metoo/party/create">创建派对</a>
 
 <style lang="scss">
   .wrapper {
     height: 100%;
     display: flex;
     flex-direction: column;
+    background-color: var(--greyscale-100);
 
     .mine-wrapper {
       height: 3.8rem;
       padding: 1.28rem;
+      margin: .68rem;
       display: flex;
+      background-color: var(--others-white);
+      border-radius: 3.18rem;
       .wt {
         width: 3.8rem;
         border-radius: 50%;
@@ -87,11 +95,39 @@
 
       .et {
         flex: 1;
-        background-color: #eef;
-        .t {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        a {
+          width: 2rem;
+          height: 2rem;
+          line-height: 2rem;
+          text-align: center;
           line-height: 1.9rem;
           font-size: 1rem;
         }
+      }
+    }
+
+    .party-wrapper {
+      flex: 1;
+      height: 0;
+
+      .header {
+        height: 1.68rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 .68rem;
+
+        h2 {
+          line-height: 1.68rem;
+          font-size: 1rem;
+        }
+      }
+      ul {
+        height: auto;
+        overflow: hidden;
       }
     }
   }
